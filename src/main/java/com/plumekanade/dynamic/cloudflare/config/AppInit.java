9397bef501e584890b4dd84d5c297ccd8d999708
plumekanade <kanade@plumekanade.com>
2022-08-30 15:21:08 +0800

    .

--- src/main/java/com/plumekanade/dynamic/cloudflare/config/AppInit.java
+++ src/main/java/com/plumekanade/dynamic/cloudflare/config/AppInit.java
@@ -2,9 +2,7 @@ package com.plumekanade.dynamic.cloudflare.config;
 
 import com.plumekanade.dynamic.cloudflare.task.DnsTask;
 import com.plumekanade.dynamic.cloudflare.task.DynamicTask;
-import com.plumekanade.dynamic.cloudflare.utils.MapperUtils;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
  * @author kanade
  * @date 2022-08-26
  */
-@Slf4j
 @Configuration
 @AllArgsConstructor
 public class AppInit implements CommandLineRunner {
@@ -25,9 +22,7 @@ public class AppInit implements CommandLineRunner {
   public void run(String... args) throws Exception {
     // 网络驱动的IP名称
     DnsTask.IP_NAME = cloudflareConfig.getIpName();
-
     // dns定时任务
     dynamicTask.startTask(DnsTask.KEY, new DnsTask(tencentConfig, cloudflareConfig), DnsTask.CRON);
-    log.info(MapperUtils.serialize(tencentConfig));
   }
 }
